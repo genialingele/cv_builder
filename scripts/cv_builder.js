27e6e4dea6bebda44b953b8ab185aac2a6a107f1
genialingele <ingelegenial@gmail.com>
2023-12-10 22:12:57 +0200

    ADD Files First Time

---
+++ scripts/cv_builder.js
@@ -0,0 +1,18 @@
+/* Copyright of GENIAL INGELE (2023). This calculator is merely an estimation of what your bond would cost and the other costs associated with it.
+    Hence, GENIAL INGELE is not liable for any inaccuracies found in this code. We are trying to guide you in making a decision in the best way we can, 
+    but the accurate information will be given to you by your bank.
+    NOTE: Data found on this code is based on South African laws and fees.
+    
+    DATE: 25 SEPTEMBER 2023
+    
+    GENIAL INGELE */
+
+
+    /*TODO 
+    * Automatically separate large numbers with comma or point when typed in
+    * Loop through 2 arrays to compare and assign values based on a set of conditions. E.g. loan amount vs deeds office fees matching. 
+    */
+
+//an Array of input fields as objects
+var input_fields = [document.getElementById(""), document.getElementById(""), document.getElementById(""), document.getElementById("")]
+
