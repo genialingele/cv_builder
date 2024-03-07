68f02042dd2b36f8496169f27dabc0745e7b9f3b
Genial Ingele <61932424+genialingele@users.noreply.github.com>
2024-03-07 21:54:29 +0000

    HIDE Irrelevant Sections are hidden only triggered with button CHANGE Styles, text, and button reactions

--- scripts/cv_builder.js
+++ scripts/cv_builder.js
@@ -14,5 +14,250 @@
     */
 
 //an Array of input fields as objects
-var input_fields = [document.getElementById(""), document.getElementById(""), document.getElementById(""), document.getElementById("")]
+var input_fields = [document.getElementById(""), 
+document.getElementById(""), document.getElementById(""), 
+document.getElementById("")];
 
+//GET THE SECTION CONAINTERS
+const personalContainer = document.getElementById("personalContainer");
+const professionalContainer = document.getElementById("professionalContainer");
+const educationContainer = document.getElementById("educationContainer");
+const skillsContainer = document.getElementById("skillsContainer");
+const languagesContainer = document.getElementById("languagesContainer");
+const certificateContainer = document.getElementById("certificateContainer");
+const resultsContainer = document.getElementById("resultsContainer");
+const progressContainer = document.getElementById("progressContainer");
+
+
+
+// LISTEN TO BUTTONS
+
+/*Now, each button will have a unique ID 
+because we each of them send you  to  specific section.
+They hide the current section and show the next one.*/
+
+const nextBtnPersonal = document.getElementById("nextBtnPersonal");
+
+const nextBtnProfessional = document.getElementById("nextBtnProfessional");
+const prevBtnProfessional = document.getElementById("prevBtnProfessional");
+
+const nextBtnEducation = document.getElementById("nextBtnEducation");
+const prevBtnEducation = document.getElementById("prevBtnEducation");
+
+const nextBtnSkill = document.getElementById("nextBtnSkill");
+const prevBtnSkill = document.getElementById("prevBtnSkill");
+
+const nextBtnLang = document.getElementById("nextBtnLang");
+const prevBtnLang = document.getElementById("prevBtnLang");
+
+const prevBtnCert = document.getElementById("prevBtnCert");
+const completeBtn = document.getElementById("completeBtn");
+
+
+
+//INITIALLY, WE WANT ALL OTHER SECTIONS TO BE HIDDEN
+function hideOtherThanPersonal (){
+    personalContainer.style.display = "block";
+    professionalContainer.style.display = "none";
+    educationContainer.style.display = "none";
+    skillsContainer.style.display = "none";
+    languagesContainer.style.display = "none";
+    certificateContainer.style.display = "none";
+    resultsContainer.style.display = "none";
+}
+
+hideOtherThanPersonal();
+
+
+//NOW WE WILL DISPLAY CONTAINERS BASED ON THE BUTTON CLICKED
+function hideOtherThanThis (containerName){
+
+    if (containerName == "personalContainer"){
+        personalContainer.style.display = "block";
+        professionalContainer.style.display = "none";
+        educationContainer.style.display = "none";
+        skillsContainer.style.display = "none";
+        languagesContainer.style.display = "none";
+        certificateContainer.style.display = "none";
+    }
+    else if (containerName == "professionalContainer"){
+        personalContainer.style.display = "none";
+        professionalContainer.style.display = "block";
+        educationContainer.style.display = "none";
+        skillsContainer.style.display = "none";
+        languagesContainer.style.display = "none";
+        certificateContainer.style.display = "none";
+    }
+    else if (containerName == "educationContainer"){
+            personalContainer.style.display = "none";
+            professionalContainer.style.display = "none";
+            educationContainer.style.display = "block";
+            skillsContainer.style.display = "none";
+            languagesContainer.style.display = "none";
+            certificateContainer.style.display = "none";
+    }
+
+    else if (containerName == "skillsContainer"){
+        personalContainer.style.display = "none";
+        professionalContainer.style.display = "none";
+        educationContainer.style.display = "none";
+        skillsContainer.style.display = "block";
+        languagesContainer.style.display = "none";
+        certificateContainer.style.display = "none";
+    }
+
+    else if (containerName == "languagesContainer"){
+        personalContainer.style.display = "none";
+        professionalContainer.style.display = "none";
+        educationContainer.style.display = "none";
+        skillsContainer.style.display = "none";
+        languagesContainer.style.display = "block";
+        certificateContainer.style.display = "none";
+    }
+
+    else if (containerName == "certificateContainer"){
+        personalContainer.style.display = "none";
+        professionalContainer.style.display = "none";
+        educationContainer.style.display = "none";
+        skillsContainer.style.display = "none";
+        languagesContainer.style.display = "none";
+        certificateContainer.style.display = "block";
+    }
+
+    else if (containerName == "certificateContainer"){
+        personalContainer.style.display = "none";
+        professionalContainer.style.display = "none";
+        educationContainer.style.display = "none";
+        skillsContainer.style.display = "none";
+        languagesContainer.style.display = "none";
+        certificateContainer.style.display = "block";
+    }
+
+
+}
+
+//NOW LET'S GET THE PARENT IDs OF BUTTONS
+const nextBtnPersonalPID = nextBtnPersonal.parentElement.parentElement.id;
+const nextBtnProfessionalPID = nextBtnProfessional.parentElement.parentElement.id;
+const nextBtnEducationPID = nextBtnEducation.parentElement.parentElement.id;
+const nextBtnSkillsPID = nextBtnSkill.parentElement.parentElement.id;
+const nextBtnLangPID = nextBtnLang.parentElement.parentElement.id;
+const prevBtnCertPID = prevBtnCert.parentElement.parentElement.id;
+const completeBtnPID = completeBtn.parentElement.parentElement.id;
+const resultsContainerPID = resultsContainer.parentElement.parentElement.id;
+
+
+//PROGRESS BAR
+//We want the color of the section tab to change when the user progresses
+
+
+
+
+
+//TRIGGER SHOWING THE NEXT OR PREVIOUS SECTION AFTER BUTTON CLICK
+
+nextBtnPersonal.addEventListener("click", 
+    function(event){
+        event.preventDefault();
+        hideOtherThanThis(nextBtnProfessionalPID);
+        
+        //IF NEXT BTN IN PERSONAL IS CLICKED
+
+    }
+
+);
+
+//BUTTONS ON THE PROFESSIONAL SECTION BRING YOU EITHER TO PERSONAL OR EDUCTION
+prevBtnProfessional.addEventListener("click", 
+    function(event){
+        event.preventDefault();
+        hideOtherThanThis(nextBtnPersonalPID);
+
+        //IF PREV BTN IN PRO IS CLICKED 
+
+    }
+)
+
+nextBtnProfessional.addEventListener("click", 
+    function(event){
+        event.preventDefault();
+        hideOtherThanThis(nextBtnEducationPID);
+        //IF NEXT BTN IN PRO IS CLICKED 
+
+    }
+
+)
+
+
+//BUTTONS ON THE EDUCATION SECTION BRING YOU EITHER TO SKILLS OR PROFESSIONAL
+prevBtnEducation.addEventListener("click", 
+    function(event){
+        event.preventDefault();
+        hideOtherThanThis(nextBtnProfessionalPID);
+        //IF PREV BTN IN EDU IS CLICKED 
+    }
+
+)
+
+nextBtnEducation.addEventListener("click", 
+    function(event){
+        event.preventDefault();
+        hideOtherThanThis(nextBtnSkillsPID);
+        //IF NEXT BTN IN EDU IS CLICKED 
+    }
+
+)
+
+
+//BUTTONS ON THE SKILLS SECTION BRING YOU EITHER TO LANGUAGES OR EDUCATION
+prevBtnSkill.addEventListener("click", 
+    function(event){
+        event.preventDefault();
+        hideOtherThanThis(nextBtnEducationPID);
+        //IF PREV BTN IN SKILL IS CLICKED 
+    }
+
+)
+
+nextBtnSkill.addEventListener("click", 
+    function(event){
+        event.preventDefault();
+        hideOtherThanThis(nextBtnLangPID);
+        //IF NEXT BTN IN SKILL IS CLICKED 
+    }
+
+)
+
+
+//BUTTONS ON THE LANGUAGES SECTION BRING YOU EITHER TO CERTIFICATES OR SKILLS
+prevBtnLang.addEventListener("click", 
+    function(event){
+        event.preventDefault();
+        hideOtherThanThis(nextBtnSkillsPID);
+    }
+
+)
+
+nextBtnLang.addEventListener("click", 
+    function(event){
+        event.preventDefault();
+        hideOtherThanThis(prevBtnCertPID);
+    }
+
+)
+
+//BUTTONS ON THE CERTIFICATES SECTION BRING YOU EITHER TO COMPLETE OR LANGUAGES
+prevBtnCert.addEventListener("click", 
+    function(event){
+        event.preventDefault();
+        hideOtherThanThis(nextBtnLangPID);
+    }
+
+)
+
+completeBtn.addEventListener("click", 
+    function(event){
+        event.preventDefault();
+        hideOtherThanThis(resultsContainerPID);
+    }
+)
